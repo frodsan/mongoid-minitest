3e8340d4c71c47956ebf03e69602dfa870220ec4
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-04-05 01:57:35 -0500

    updating have_field matcher

--- lib/minitest/matchers/have_field.rb
+++ lib/minitest/matchers/have_field.rb
@@ -7,7 +7,29 @@ module Mongoid
         end
 
         def matches?(klass)
-          klass.fields.include?(@field)
+          @klass  = klass.is_a?(Class) ? klass : klass.class
+          @errors = []
+
+          if @klass.fields.include?(@field)
+          else
+            @errors.push "no field named #{@field}"
+          end
+
+          @errors.empty?
+        end
+
+        def failure_message
+          "Expected #{@klass.inspect} to #{description}, got #{@errors.to_sentence}"
+        end
+
+        def negative_failure_message
+          msg =  "Expected #{@klass.inspect} to not #{description}, "
+          msg << "got #{@klass.inspect} to #{description}"
+        end
+
+        def description
+          desc = "have field named #{@field}"
+          desc
         end
       end
 
--- test/matchers/have_field_test.rb
+++ test/matchers/have_field_test.rb
@@ -1,9 +1,47 @@
 require "test_helper"
 
-describe "Document matchers" do
+describe "HaveFieldMatcher" do
+  describe "#must" do
+    it "passes if the field exists" do
+      assert_must Dummy, have_field(:name)
+      Dummy.must have_field(:name)
+    end
 
-  it "should assert to true if field exists" do
-    assert_must Dummy, have_field(:name)
-    Dummy.must have_field(:name)
+    it "raises an error when the field doesn't exist" do
+      assert_failure(failure_message) do
+        assert_must Dummy, have_field(:noexist)
+      end
+
+      assert_failure(failure_message) do
+        Dummy.must have_field(:noexist)
+      end
+    end
+  end
+
+  describe "#wont" do
+    it "passes if the field doesn't exist" do
+      assert_wont Dummy, have_field(:noexist)
+      Dummy.wont have_field(:noexist)
+    end
+
+    it "raises an error when the field exists" do
+      assert_failure(negative_failure_message) do
+        assert_wont Dummy, have_field(:name)
+      end
+
+      assert_failure(negative_failure_message) do
+        Dummy.wont have_field(:name)
+      end
+    end
+  end
+
+  private
+
+  def failure_message
+    "Expected Dummy to have field named noexist, got no field named noexist."
+  end
+
+  def negative_failure_message
+    "Expected Dummy to not have field named name, got Dummy to have field named name."
   end
 end
