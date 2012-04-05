1f0d7d7acde9b15759dd4d628b14823cbd92c05e
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-04-05 13:03:57 -0500

    removing unnecessary lines

--- lib/mongoid-minitest.rb
+++ lib/mongoid-minitest.rb
@@ -1,9 +1,6 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-MATCHERS = "/mongoid-minitest/matchers/*.rb"
-
 gem "minitest"
 require "mongoid"
 require "minitest/spec"
 
 require "minitest/matchers"
-Dir[File.dirname(__FILE__) + MATCHERS].each { |f| require f }
+Dir[File.dirname(__FILE__) + "/mongoid-minitest/matchers/*.rb"].each { |f| require f }
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,3 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "lib"))
-DUMMY = File.join(File.dirname(__FILE__), "dummy")
-$LOAD_PATH.unshift(DUMMY)
-
 gem "minitest"
 require "mongoid"
 require "minitest/spec"
@@ -12,7 +7,7 @@ Mongoid.configure do |config|
   config.master = Mongo::Connection.new.db("mongoid-minitest-test")
 end
 
-Dir[File.join(DUMMY, "*.rb")].sort.each { |file| require File.basename(file) }
+Dir[File.dirname(__FILE__) + "/dummy/*.rb"].each { |f| require f }
 
 require "mongoid-minitest"
 
