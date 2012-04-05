95b4155f1e34bee08e7f637bddcea75db2cc6a6c
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-04-05 16:55:06 -0500

    testing like @goddamnhippie => simple

--- test/matchers/have_field_test.rb
+++ test/matchers/have_field_test.rb
@@ -1,55 +1,8 @@
-# encoding: utf-8
 require "test_helper"
 
 describe "HaveFieldMatcher" do
-  describe "#must" do
-    it "passes if the field exists" do
-      assert_must Dummy, have_field(:name)
-      Dummy.must have_field(:name)
-    end
+  subject { Dummy }
 
-    it "raises an error when the field doesn't exist" do
-      assert_failure(failure_message) do
-        assert_must Dummy, have_field(:noexist)
-      end
-
-      assert_failure(failure_message) do
-        Dummy.must have_field(:noexist)
-      end
-    end
-  end
-
-  describe "#wont" do
-    it "passes if the field doesn't exist" do
-      assert_wont Dummy, have_field(:noexist)
-      Dummy.wont have_field(:noexist)
-    end
-
-    it "raises an error when the field exists" do
-      assert_failure(negative_failure_message) do
-        assert_wont Dummy, have_field(:name)
-      end
-
-      assert_failure(negative_failure_message) do
-        Dummy.wont have_field(:name)
-      end
-    end
-  end
-
-  describe "with subject" do
-    subject { Dummy }
-
-    it { must have_field(:name) }
-    it { wont have_field(:noexist) }
-  end
-
-  private
-
-  def failure_message
-    "Expected Dummy to have field named noexist, got no field named noexist."
-  end
-
-  def negative_failure_message
-    "Expected Dummy to not have field named name, got Dummy to have field named name."
-  end
+  it { must have_field(:name) }
+  it { wont have_field(:noexist) }
 end
