dd19c008f8ee9bf1ab4892020fc7bdf2e0f372be
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-04-10 11:57:13 -0500

    better organization for document matchers

--- lib/mongoid-minitest.rb
+++ lib/mongoid-minitest.rb
@@ -3,4 +3,4 @@ require "mongoid"
 require "minitest/spec"
 
 require "minitest/matchers"
-Dir[File.dirname(__FILE__) + "/mongoid-minitest/matchers/*.rb"].each { |f| require f }
+Dir[File.dirname(__FILE__) + "/mongoid-minitest/matchers/**/*.rb"].each { |f| require f }
