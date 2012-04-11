1193182b49114c8a229c13633e988962833ff364
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-04-11 08:05:17 -0500

    removing unnecessary tests

--- test/matchers/validations_test.rb
+++ test/matchers/validations_test.rb
@@ -6,13 +6,9 @@ describe Mongoid::Matchers::Validations do
   it { must validate_presence_of(:name) }
   it { wont validate_presence_of(:age) }
 
-  it { must validate_uniqueness_of(:login) }
-  it { wont validate_uniqueness_of(:name) }
   it { must validate_uniqueness_of(:login).case_insensitive }
   it { wont validate_uniqueness_of(:code).case_insensitive }
 
-  it { must validate_length_of(:password) }
-  it { wont validate_length_of(:code) }
   it { must validate_length_of(:password).with_min(8) }
   it { wont validate_length_of(:password).with_min(0) }
   it { must validate_length_of(:password).with_minimum(8) }
