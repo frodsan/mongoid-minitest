08c5b3f7e06e82adf4ad06be6b438c172d3bc381
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-02-27 02:25:37 -0500

    Revert "extract validation matchers to minitest-activemodel"
    This was a test. Revert and create a new branch.
    
    This reverts commit 5202ae7873932ac4021659055a96f1d920663978.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,3 @@
 source 'https://rubygems.org'
 
 gemspec
-
-gem 'minitest-activemodel', github: 'frodsan/minitest-activemodel'
--- lib/mongoid-minitest.rb
+++ lib/mongoid-minitest.rb
@@ -1,13 +1,19 @@
 gem 'minitest'
-require 'minitest-activemodel'
+require 'minitest/matchers'
 require 'matchers/matcher'
 require 'matchers/document/document'
 require 'matchers/document/have_field'
 require 'matchers/document/be_stored_in'
 require 'matchers/document/have_index'
 require 'matchers/validations/validations'
+require 'matchers/validations/presence'
+require 'matchers/validations/uniqueness'
+require 'matchers/validations/length'
+require 'matchers/validations/format'
 require 'matchers/validations/inclusion'
 require 'matchers/validations/exclusion'
+require 'matchers/validations/confirmation'
+require 'matchers/validations/acceptance'
 require 'matchers/validations/associated'
 require 'matchers/associations/associations'
 
@@ -16,6 +22,5 @@ module Mongoid
     include Document
     include Validations
     include Associations
-    include MiniTest::Matchers::ActiveModel
   end
 end
